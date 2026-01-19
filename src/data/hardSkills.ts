92f00ee7b1965fc295f2f197e0f3fb04c539d0c1
amin1148 <amin23-1148@diu.edu.bd>
2026-01-19 18:32:56 +0600

    Update hardSkills.ts

--- src/data/hardSkills.ts
+++ src/data/hardSkills.ts
@@ -6,25 +6,10 @@ interface HardSkill {
 
 const hardSkills: HardSkill[] = [
   {
-    name: "AstroJs",
-    description: "My favorite static site generator for building modern websites",
-    icon: "astro_dark"
+    name: "Garment Merchandising",
+    description: "Expertise in Tech Packs, T&A, Costing, Fabric Consumption, Lab-Dips, Strike-offs, and end-to-end Production (Marker to Shipment).",
+    icon: "astro_dark" 
   },
-  {
-    name: "NextJs",
-    description: "My favorite React framework for building modern web apps",
-    icon: "nextjs_icon_dark"
-  },
-  {
-    name: "NodeJs",
-    description: "I've been using NodeJs for backend development since 2018",
-    icon: "nodejs"
-  },
-  {
-    name: "MongoDB",
-    description: "My favorite database for modern apps",
-    icon: "mongodb"
-  }
 ];
 
 export default hardSkills;
