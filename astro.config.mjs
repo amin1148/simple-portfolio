b8b466b3b13e4e84af61f89e345c0476a89aed0c
amin1148 <amin23-1148@diu.edu.bd>
2026-01-19 19:30:44 +0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
 // @ts-check
 import {defineConfig} from 'astro/config';
 import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://simple-portfolio.vicbox.dev/',
+    // 1. Your GitHub Pages domain
+    site: 'https://amin1148.github.io',
+
+    // 2. Your Repository Name (Critical for CSS/Images to work)
+    base: '/simple-portfolio',
+
     integrations: [mdx(), react(), tailwind({
         applyBaseStyles: false,
     }), icon(), sitemap()],
@@ -24,4 +25,5 @@ export default defineConfig({
     },
     experimental: {
         svg: true,
-    }});
+    }
+});
