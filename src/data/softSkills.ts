18e1072f0b6b557c2e3d08a6115d1ef053bea356
amin1148 <amin23-1148@diu.edu.bd>
2026-01-19 18:39:02 +0600

    Update softSkills.ts

--- src/data/softSkills.ts
+++ src/data/softSkills.ts
@@ -1,25 +1,41 @@
 interface SoftSkill {
-    name: string;
-    icon: string;
+  name: string;
+  icon: string;
 }
 
 const softSkills: SoftSkill[] = [
-    {
-        name: "Solving Problems",
-        icon: "tools-fill"
-    },
-    {
-        name: "Teamwork",
-        icon: "group-fill"
-    },
-    {
-        name: "Communication",
-        icon: "message-2-fill"
-    },
-    {
-        name: "Time Management",
-        icon: "time-fill"
-    }
+  {
+    name: "Problem Solving",
+    icon: "tools-fill"
+  },
+  {
+    name: "Communication",
+    icon: "message-2-fill"
+  },
+  {
+    name: "Negotiation", // Crucial for costing and timelines
+    icon: "shake-hands-fill"
+  },
+  {
+    name: "Time Management",
+    icon: "time-fill"
+  },
+  {
+    name: "Team Player",
+    icon: "group-fill"
+  },
+  {
+    name: "Attention to Detail", // Vital for Tech Packs and Quality
+    icon: "eye-fill"
+  },
+  {
+    name: "Adaptability", // For sudden changes in trends or production issues
+    icon: "refresh-fill"
+  },
+  {
+    name: "Email Proficiency",
+    icon: "mail-send-fill" // Updated icon name to be more specific
+  }
 ];
 
 export default softSkills;
